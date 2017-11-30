edeb9db061c3af31551cc43dd7e3f195e043ed45
vetkolisanket <vetkolisanket@gmail.com>
2017-11-30 20:32:56 +0530

    Initial commit

---
+++ app/src/main/java/com/vishwas/credit/standapp/ui/activities/BaseActivity.java
@@ -0,0 +1,10 @@
+package com.vishwas.credit.standapp.ui.activities;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Sanket on 30/11/17.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+}
---
+++ app/src/main/java/com/vishwas/credit/standapp/ui/activities/SplashActivity.java
@@ -0,0 +1,19 @@
+package com.vishwas.credit.standapp.ui.activities;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.vishwas.credit.standapp.R;
+
+/**
+ * Created by Sanket on 30/11/17.
+ */
+
+public class SplashActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
