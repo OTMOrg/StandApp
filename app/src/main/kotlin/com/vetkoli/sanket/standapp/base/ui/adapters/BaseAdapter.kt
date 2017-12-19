5826c87ff0fb033eb8d5f5b5c9a6e2efd000332d
vetkolisanket <vetkolisanket@gmail.com>
2017-12-19 22:34:35 +0530

    Completed the BaseAdapter functionality, now my Recycler View Adapter is completely abstract and will require almost no boiler plate code

---
+++ app/src/main/kotlin/com/vetkoli/sanket/standapp/base/ui/adapters/BaseAdapter.kt
@@ -0,0 +1,36 @@
+package com.vetkoli.sanket.standapp.base.ui.adapters
+
+import android.support.v7.widget.RecyclerView
+import android.view.ViewGroup
+import com.vetkoli.sanket.standapp.base.ui.viewholders.BaseViewHolder
+import com.vetkoli.sanket.standapp.utils.unsafeLazy
+
+/**
+ * Created by Sanket on 19/12/17.
+ */
+abstract class BaseAdapter<T> : RecyclerView.Adapter<BaseViewHolder>() {
+
+    private var itemList: MutableList<T> = mutableListOf()
+
+    protected val delegationManager: BaseAdapterDelegatesManager<T>
+            by unsafeLazy { BaseAdapterDelegatesManager<T>() }
+
+    protected fun setItems(itemList: List<T>) {
+        this.itemList.addAll(itemList)
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder {
+        return delegationManager.onCreateViewHolder(parent, viewType)
+    }
+
+    override fun getItemCount(): Int {
+        return itemList.size
+    }
+
+    override fun onBindViewHolder(holder: BaseViewHolder, position: Int) {
+        delegationManager.onBindViewHolder(holder, itemList.get(position))
+    }
+
+
+}
--- app/src/main/kotlin/com/vetkoli/sanket/standapp/home/ui/adapters/MembersAdapter.kt
+++ app/src/main/kotlin/com/vetkoli/sanket/standapp/home/ui/adapters/MembersAdapter.kt
@@ -1,36 +1,15 @@
 package com.vetkoli.sanket.standapp.home.ui.adapters
 
-import android.support.v7.widget.RecyclerView
-import android.view.ViewGroup
 import com.vetkoli.sanket.standapp.base.models.Item
-import com.vetkoli.sanket.standapp.base.ui.adapters.BaseAdapterDelegatesManager
-import com.vetkoli.sanket.standapp.base.ui.viewholders.BaseViewHolder
-import com.vetkoli.sanket.standapp.models.Member
-import com.vetkoli.sanket.standapp.utils.unsafeLazy
+import com.vetkoli.sanket.standapp.base.ui.adapters.BaseAdapter
 
 /**
  * Created by Sanket on 16/12/17.
  */
-class MembersAdapter(var memberList: MutableList<Member>) : RecyclerView.Adapter<BaseViewHolder>() {
-
-    private val delegationManager: BaseAdapterDelegatesManager<Item>
-            by unsafeLazy { BaseAdapterDelegatesManager<Item>() }
+class MembersAdapter(memberList: MutableList<Item>) : BaseAdapter<Item>() {
 
     init {
         delegationManager.addDelegate(MemberAdapterDelegate())
+        setItems(memberList)
     }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder {
-        return delegationManager.onCreateViewHolder(parent, viewType)
-    }
-
-    override fun getItemCount(): Int {
-        return memberList.size
-    }
-
-    override fun onBindViewHolder(holder: BaseViewHolder, position: Int) {
-        delegationManager.onBindViewHolder(holder, memberList.get(position))
-    }
-
-
 }
