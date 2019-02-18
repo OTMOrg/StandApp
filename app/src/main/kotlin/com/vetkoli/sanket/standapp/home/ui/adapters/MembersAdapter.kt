506e1b8967fe64dd021019a6d462c65900e42bcc
Sanket <vetkolisanket@gmail.com>
2019-02-18 22:00:26 +0530

    Removed delegation manager from adapter

--- app/src/main/kotlin/com/vetkoli/sanket/standapp/home/ui/adapters/MembersAdapter.kt
+++ app/src/main/kotlin/com/vetkoli/sanket/standapp/home/ui/adapters/MembersAdapter.kt
@@ -1,15 +1,26 @@
 package com.vetkoli.sanket.standapp.home.ui.adapters
 
-import com.vetkoli.sanket.standapp.base.ui.adapters.BaseAdapter
+import android.support.v7.widget.RecyclerView
+import android.view.ViewGroup
+import com.vetkoli.sanket.standapp.R
 import com.vetkoli.sanket.standapp.models.Member
+import com.vetkoli.sanket.standapp.utils.inflate
 
 /**
  * Created by Sanket on 16/12/17.
  */
-class MembersAdapter(memberList: MutableList<Member>) : BaseAdapter<Member>() {
+class MembersAdapter(var memberList: MutableList<Member>) : RecyclerView.Adapter<MemberViewHolder>() {
 
-    init {
-        delegationManager.addDelegate(MemberAdapterDelegate())
-        setItems(memberList)
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): MemberViewHolder {
+        return MemberViewHolder(parent!!.inflate(R.layout.item_member))
     }
+
+    override fun getItemCount(): Int {
+        return memberList.size
+    }
+
+    override fun onBindViewHolder(holder: MemberViewHolder?, position: Int) {
+        holder?.bind(memberList[position])
+    }
+
 }
